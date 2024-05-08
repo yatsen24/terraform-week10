d69aa6d0cbb37e746ca67d53dce792084fc107bc
yatsen24 <158105724+yatsen24@users.noreply.github.com>
2024-05-07 19:57:00 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "week10-mb-terraform"
+    bucket         = "week10-mk-terraform"
     key            = "test/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
